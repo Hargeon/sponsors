c21824bf1b6cc4d21ec35102d042b2bf400d2751
Ilya Dzemyanets <hirami14590@gmail.com>
2019-12-03 20:07:15 +0300

    add tests for model Idea

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,4 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
+  context 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:industry) }
+    it { should validate_presence_of(:focus) }
+    it { should validate_presence_of(:plan) }
+
+    it { should validate_length_of(:name).is_at_least(5) }
+    it "count of description's words less 10" do
+      idea = Idea.new(name: 'Simple name', industry: 1, focus: 1,
+                      plan: 'One two three foor five six seven eight nine ten',
+                      description: 'One two three')
+      expect(idea).to eq(false)
+    end
+
+    it "count of description's words equals 10" do
+      idea = Idea.new(name: 'Simple name', industry: 1, focus: 1,
+                      plan: 'One two three foor five six seven eight nine ten',
+                      description: 'One two three foor five six seven eight nine ten')
+      expect(idea).to eq(true)
+    end
+
+    it "count of description's words more 10" do
+      idea = Idea.new(name: 'Simple name', industry: 1, focus: 1,
+                      plan: 'One two three foor five six seven eight nine ten',
+                      description: 'One two three foor five six seven eight nine ten eleven')
+      expect(idea).to eq(true)
+    end
+
+    it "count of plan's words less 10" do
+      idea = Idea.new(name: 'Simple name', industry: 1, focus: 1,
+                      plan: 'One two three foor five six seven eight',
+                      description: 'One two three foor five six seven eight nine ten')
+      expect(idea).to eq(false)
+    end
+
+    it "count of plan's words equals 10" do
+      idea = Idea.new(name: 'Simple name', industry: 1, focus: 1,
+                      plan: 'One two three foor five six seven eight nine ten',
+                      description: 'One two three foor five six seven eight nine ten')
+      expect(idea).to eq(true)
+    end
+
+    it "count of plan's words more 10" do
+      idea = Idea.new(name: 'Simple name', industry: 1, focus: 1,
+                      plan: 'One two three foor five six seven eight nine ten eleven',
+                      description: 'One two three foor five six seven eight nine ten')
+      expect(idea).to eq(true)
+    end
+  end
 end
