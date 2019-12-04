cb27e4dff8e9e785620ff0dd6ce307f2dfba3eda
Ilya Dzemyanets <hirami14590@gmail.com>
2019-12-04 16:07:27 +0300

    change idea table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_124207) do
+ActiveRecord::Schema.define(version: 2019_12_04_130419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,6 @@ ActiveRecord::Schema.define(version: 2019_12_04_124207) do
   create_table "ideas", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "industry"
-    t.integer "focus"
     t.text "plan"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
