67351eaaf1fbc764642112591b7ab76496b877e5
Ilya Dzemyanets <hirami14590@gmail.com>
2019-12-04 18:07:53 +0300

    add constants to idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,7 @@
 class Idea < ApplicationRecord
+  COUNT_WORDS_OF_PLAN = 10
+  COUNT_WORDS_OF_DESCRIPTION = 10
   validates :name, length: { minimum: 4 }
-  validates :plan, size: true
-  validates :description, size: true
+  validates :plan, count_words: { minimum: COUNT_WORDS_OF_PLAN }
+  validates :description, count_words: { minimum: COUNT_WORDS_OF_DESCRIPTION }
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -9,17 +9,12 @@ RSpec.describe Idea, type: :model do
 
     it { should validate_length_of(:name).is_at_least(4) }
 
-    it 'count words of description equals 10' do
-      expect(idea.valid?).to eq(true)
-    end
-
-    it 'count words of plan equals 10' do
+    it 'should be valid' do
       expect(idea.valid?).to eq(true)
     end
 
     context 'description has less than 10 words' do
       let(:description) { 'one two three fout five six seven eight nine' }
-      let(:idea) { Idea.new(name: name, description: description, plan: plan) }
 
       it 'should be invalid' do
         expect(idea.valid?).to eq(false)
@@ -28,7 +23,6 @@ RSpec.describe Idea, type: :model do
 
     context 'plan has less than 10 words' do
       let(:plan) { 'one two three fout five six seven eight nine' }
-      let(:idea) { Idea.new(name: name, description: description, plan: plan) }
 
       it 'should be invalid' do
         expect(idea.valid?).to eq(false)
