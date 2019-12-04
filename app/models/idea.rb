4a410edacbcbb45cc6ccf8094120a8f9a6b580aa
Ilya Dzemyanets <hirami14590@gmail.com>
2019-12-04 18:13:22 +0300

    add constant for length of name

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,9 @@
 class Idea < ApplicationRecord
   COUNT_WORDS_OF_PLAN = 10
   COUNT_WORDS_OF_DESCRIPTION = 10
-  validates :name, length: { minimum: 4 }
+  MINIMUM_LENGTH_OF_NAME = 4
+
+  validates :name, length: { minimum: MINIMUM_LENGTH_OF_NAME }
   validates :plan, count_words: { minimum: COUNT_WORDS_OF_PLAN }
   validates :description, count_words: { minimum: COUNT_WORDS_OF_DESCRIPTION }
 end
