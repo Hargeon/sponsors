ea30b3ac9bdc744f2d672cb066a749301b9b3a83
Ilya Dzemyanets <hirami14590@gmail.com>
2019-12-11 15:34:03 +0300

    add spec for idaes controller

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe IdeasController, type: :controller do
+  let(:name) { 'Simple name' }
+  let(:description) { 'one two three fout five six seven eight nine ten' }
+  let(:plan) { 'one two three fout five six seven eight nine ten' }
+  let(:idea) { Idea.create(name: name, description: description, plan: plan) }
 
+  describe 'GET index' do
+    it 'has a 200 status code' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'GET show' do
+    it 'has a 200 status code' do
+      get :show, params: { id: idea.id }
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'GET new' do
+    it 'has a 200 status code' do
+      get :new
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'POST new' do
+    it 'has a 200 status code' do
+      post :create, params: { idea: { name: name, description: description, plan: plan,
+                              local_industries: [1], local_districts: [1], local_members: [1],
+                              local_require_helps: [1] } }
+      expect(response.status).to eq(200)
+    end
+  end
 end
