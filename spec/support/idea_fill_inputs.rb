3b5249794aef577a3665c59290fbd8ae71bfbfc2
Ilya Dzemyanets <hirami14590@gmail.com>
2019-12-30 12:35:04 +0300

    add shared example and refactor tests

--- spec/support/idea_fill_inputs.rb
+++ spec/support/idea_fill_inputs.rb
@@ -2,6 +2,13 @@ RSpec.shared_examples 'idea fill inputs' do
   describe 'fill inputs' do
     let(:amount) { -1 }
 
+    shared_examples 'page has correct message' do |message|
+      scenario "should dispaly #{message}" do
+        click_button 'commit'
+        expect(page).to have_content(message)
+      end
+    end
+
     feature 'with correct params' do
       scenario 'should redirect to show' do
         click_button 'commit'
@@ -11,47 +18,59 @@ RSpec.shared_examples 'idea fill inputs' do
     end
 
     feature 'with incorrect name' do
-      scenario 'should display name error' do
-        fill_and_expect_content('Name is too short (minimum is 4 characters)', 'Name')
+      before do
+        fill_in('Name', with: '')
       end
+
+      include_examples 'page has correct message', 'Name is too short (minimum is 4 characters)', 'Name'
     end
 
     feature 'with incorrect description' do
-      scenario 'should display description error' do
-        fill_and_expect_content('Description description need has at least 10 words', 'Description')
+      before do
+        fill_in('Description', with: '')
       end
+
+      include_examples 'page has correct message', 'Description need has at least 10 words'
     end
 
     feature 'without members' do
-      scenario 'should display member error' do
+      before do
         click_button Member.first.name
-        expect_content("Local members can't be blank")
       end
+
+      include_examples 'page has correct message', "Local members can't be blank"
     end
 
     feature 'with incorrect amount of members' do
-      scenario 'should display amount error' do
-        fill_and_expect_content('Local members amount must be greater than or equal to 0',
-                                'idea[local_members_attributes][][amount]', amount)
+      before do
+        fill_in 'idea[local_members_attributes][][amount]', with: amount
       end
+
+      include_examples 'page has correct message', 'Local members amount must be greater than or equal to 0'
     end
 
     feature 'without industries' do
-      scenario 'should display local industry error' do
-        unselect_and_expect_content("Local industries can't be blank", 'idea_local_industries')
+      before do
+        unselect 'Some name', from: 'idea_local_industries'
       end
+
+      include_examples 'page has correct message', "Local industries can't be blank"
     end
 
     feature 'without districts' do
-      scenario 'should display local district error' do
-        unselect_and_expect_content("Local districts can't be blank", 'idea_local_districts')
+      before do
+        unselect 'Some name', from: 'idea_local_districts'
       end
+
+      include_examples 'page has correct message', "Local districts can't be blank"
     end
 
     feature 'without require helps' do
-      scenario 'should display local require help error' do
-        unselect_and_expect_content("Local require helps can't be blank", 'idea_local_require_helps')
+      before do
+        unselect 'Some name', from: 'idea_local_require_helps'
       end
+
+      include_examples 'page has correct message', "Local require helps can't be blank"
     end
   end
 end
--- spec/support/idea_helper.rb
+++ spec/support/idea_helper.rb
@@ -20,19 +20,4 @@ module IdeaHelper
     create(:require_help)
     create(:member)
   end
-
-  def expect_content(content)
-    click_button 'commit'
-    expect(page).to have_content(content)
-  end
-
-  def fill_and_expect_content(content, field, value = '')
-    fill_in field, with: value
-    expect_content(content)
-  end
-
-  def unselect_and_expect_content(content, from)
-    unselect 'Some name', from: from
-    expect_content(content)
-  end
 end
