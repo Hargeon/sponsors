d33636b09a15db430c1d7876fdcc2a6bc5663308
Ilya Dzemyanets <hirami14590@gmail.com>
2020-02-04 01:57:01 +0300

    config I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:alert] = exception.message
     redirect_to root_path
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,26 @@ require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  resources :ideas do
-    member do
-      post 'update_active_time'
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    devise_for :users, controllers: { registrations: 'users/registrations' }
+    devise_for :admin_users, ActiveAdmin::Devise.config
+    ActiveAdmin.routes(self)
+    resources :ideas do
+      member do
+        post 'update_active_time'
+      end
+
+      collection do
+        post 'search'
+        post 'filter'
+      end
     end
 
-    collection do
-      post 'search'
-      post 'filter'
-    end
+    resources :interests, only: [:create, :new]
+    resources :sponsors, only: [:show, :edit, :update]
+    resources :businessmen, only: [:show]
+    root 'ideas#index'
   end
-
-  resources :interests, only: [:create, :new]
-  resources :sponsors, only: [:show, :edit, :update]
-  resources :businessmen, only: [:show]
-  root 'ideas#index'
   mount Sidekiq::Web => '/sidekiq'
   namespace 'api' do
     namespace 'v1' do
