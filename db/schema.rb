64069e7896e032d8bbd3e34e2b012d295fcd0019
Ilya Dzemyanets <hirami14590@gmail.com>
2020-02-19 01:20:20 +0300

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -196,11 +196,11 @@ ActiveRecord::Schema.define(version: 2020_02_17_134255) do
     t.integer "age"
     t.integer "user_type"
     t.string "locale"
+    t.string "provider"
+    t.string "uid"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
