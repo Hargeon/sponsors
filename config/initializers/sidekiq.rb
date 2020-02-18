979c254c76be2a4897172a898a16dd343d6020d0
Ilya Dzemyanets <hirami14590@gmail.com>
2020-02-18 14:47:48 +0300

    add redis to go

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV['REDISTOGO_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV['REDISTOGO_URL'] }
 end
 
 schedule_file = 'config/schedule.yml'
