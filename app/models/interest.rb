0c31e856dafa77bbd12804d0e589f4847e1661fa
Ilya Dzemyanets <hirami14590@gmail.com>
2020-02-24 12:49:43 +0300

    add validates to interests message

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -3,8 +3,10 @@ class Interest < ApplicationRecord
   belongs_to :user
 
   COUNT_WORDS_OF_MESSAGE = 10
+  MAXIMUM_LENGTH_OF_MESSAGE = 100
 
   validates :message, count_words: { minimum: COUNT_WORDS_OF_MESSAGE }
+  validates :message, length: { maximum: MAXIMUM_LENGTH_OF_MESSAGE }
   validates_uniqueness_of :user_id, scope: :idea_id
   validates :user_id, :idea_id, presence: true
 end
